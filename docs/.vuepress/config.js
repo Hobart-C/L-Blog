6361b5be3e121b039a56c8f73e06c05982c17f81
Hobart <yiliang.chen@einyun.com>
2021-11-25 18:08:51 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  head: [
+    [
+      'link', // 设置 favicon.ico，注意图片放在 public 文件夹下
+      { rel: 'icon', href: 'images/hero.png' }
+    ]
+  ],
+  title: 'L-BLOG',
+  description: '个人技术博客'
+}
