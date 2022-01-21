fa9a3fd159dc7da32fae43f60a293efa33106f48
Hobart <yiliang.chen@einyun.com>
2022-01-21 18:01:26 +0800

    提交master

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
-
 # 确保脚本抛出遇到的错误
 set -e
 
 git add -A
 git commit -m '提交master'
 
-git push -f git@github.com:Hobart-C/L-Blog.git master
+git push -f git@github.com:Hobart-C/L-Blog.git
 
 
 # 生成静态文件
@@ -27,7 +26,8 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git push -f git@github.com:Hobart-C/L-Blog.git master:docs
+git push -f git@github.com:Hobart-C/L-Blog.git master:docs
 exec /bin/bash
 
 cd -
+
